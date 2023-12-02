63ad051fe7d8f5598fc63c3fa4f47cb921956599
Herimanjaka <geit.herimanjaka@gmail.com>
2023-12-02 23:15:34 +0300

    ajout de description du fonction

--- fonction.py
+++ fonction.py
@@ -1,6 +1,14 @@
 from bs4 import BeautifulSoup
 import requests
 
-def get_BeautifulSoup(url):
+def get_BeautifulSoup(url:str):
+    """scrapp un url fournit en argument et retourne un Objet Beautifulsoup.
+
+    Args:
+        url (str): url a scrapper
+
+    Returns:
+        BeautifulSoup: Objet qui fournit des methodes pour naviguer et chercer dans l'arborescence du page.
+    """
     res = requests.get(url)
     return BeautifulSoup(res.text,'html.parser')
