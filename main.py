1985dbc0f45f3bbdac72543c37981597fdaf3d03
Herimanjaka <geit.herimanjaka@gmail.com>
2023-12-02 21:10:19 +0300

    ajustement du main avec la fonction creee

--- main.py
+++ main.py
@@ -1,24 +1,32 @@
-from scrappy import result,top_tags
+from fonction import *
 
 if __name__ == "__main__":
+    url = "https://quotes.toscrape.com/"
+    print("\nChargement ...\n")
+    soup = get_BeautifulSoup(url)
+    top_tags = [tag.string for tag in soup.find('div','tags-box').find_all('a','tag')]
     print("""
     *********************************
           Ce programme vous 
           offre des citations 
           de votre choix
+
     *********************************
+          
 """)
             
     print("Ci-dessous la liste de top tag :")
     print(top_tags)
+    print("\n")
     user_tags = input("Veuillez saisir votre tags: ")
-    find = False
-
-    for quote in result:
-        if user_tags in [tag.string for tag in quote.find_all('a','tag')] :
-            print('\n')
-            print(quote.find('span','text').string)
-            find = True
+    print("\n Chargement ... \n")
     
-    if not find:
-        print('Tag non trouve dans cette page')
+    soup = get_BeautifulSoup(url+ f"/tag/{user_tags}")
+
+    if len(soup.find_all('div','quote')) > 0:
+        for quote in soup.find_all('div','quote'):
+            if user_tags in [tag.string for tag in quote.find_all('a','tag')] :
+                print('\n')
+                print(quote.find('span','text').string)
+    else:
+        print("Y a pas de citations qui correspond a votre tag")
